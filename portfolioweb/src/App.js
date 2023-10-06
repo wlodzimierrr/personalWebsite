581433fc294ed0036da869175d3cf3d93af7c469
wlodzimierrr <wlodzimierrr@github.com>
2023-10-06 15:03:13 +0100

    updated html and css, heading added in html

--- portfolioweb/src/App.js
+++ portfolioweb/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
+
+const styles = makeStyles({
+  
+})
+
 
 function App() {
+  const classes = styles(); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={`${classes.heading}`}>
+            <h1 class="heading">Wojciech Bak</h1>
       </header>
     </div>
   );
